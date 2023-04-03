00220e5158f055d03a2a75759db38e89776444b3
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-04-03 22:25:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+ const employee = {
+    name: "Christopher Smith",
+    streetAddress: "70 LinconStreet"
+ };
+ function updateEmployeeWithKeyAndValue(object,key,value) {
+    const updatedObject = {...object};
+    updatedObject[key] = value;
+    return updatedObject;
+
+ }function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value) {
+    object[key] = value;
+    return object;
+ }
+ function deleteFromEmployeeByKey(object,key) {
+    const newObject = {...object};
+    delete newObject[key];
+    return newObject;
+ }
+ function destructivelyDeleteFromEmployeeByKey(object,key) {
+    delete object[key];
+    return object;
+ }
